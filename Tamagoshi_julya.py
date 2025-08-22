5182eddcaba4e79a6416f1a8f34bc22e43a99b53
JULYA JACOMETTI <julya.jacometti2@senaisp.edu.br>
2025-08-22 14:15:29 -0300

    Em progresso

--- Tamagoshi_julya.py
+++ Tamagoshi_julya.py
@@ -5,23 +5,29 @@
 # definição de função de vida, tempo (yasme)
 # chamar todas as funções para funcionamento do jogo e incluir coisas que você alterar
 
+# ====== Listas para o funcionamento do jogo ======
+                    # Água            Terra               Ar                Fogo               
+listaBrincadeiras = ["Ir na piscina", "Castelo de areia", "Soltar pipa", "Jogar queimada", "Video game", "Cancelar escolha"]
+listaComidas = ["Gelatina", "Batata", "Pipoca", "Pimenta", "Sorvete", "Cancelar alimentação"]
+listaAcao = [f"Alimentar", "Brincar", "Passar o tempo", "Necessidade da raça", "Status do pet", "Sair (o pet irá dormir)"]
+
 
 # ====== Classe mãe ======
 class Tamagoshi:
     def __init__(self, nome):
         self.nome = nome
-        self.fome = 0 
+        self.fome = 50 
         self.saude = 100
         self.idade = 0
         self.tedio = 0
         self.dormindo = False
         
     def statusPet(self):
-        print(f"\n{25 * '='}\nSTATUS DE {self.nome}")
+        print(f"\n{25 * '='}\nSTATUS DE: {self.nome}")
         print(f"😋 Fome: {self.fome}")
         print(f"😷 Saúde: {self.saude}")
         print(f"🙄 Tédio: {self.tedio}")
-        print(f"🤔 Humor: {self.humor()}")
+        self.humor()
         print(f"📏 Idade: {self.idade}")
         if isinstance(self, Aquati):
             print(f"💧 Hidratação: {self.hidratacao}")
@@ -29,64 +35,39 @@ class Tamagoshi:
             print(f"Calor: {self.calor}")
             
             
-    def acoes(self):
-        listaAcao = [f"1 - 🥗 Alimentar", "2 - 🕹️ Brincar", "3- ⏳ Passar o tempo", "Necessidade da raça", "5 - ❌ Sair (o pet irá dormir)"]
-        
+    def acoes(self): 
         if isinstance(self, Aquati):
             listaAcao[3] = "4 - 💧 Hidratar"
         elif isinstance(self, Fuegui):
             listaAcao[3] = "4 - 🔥 Esquentar"
         
         
-        print(listaAcao)
         
 
 
     # Verifica as condições da brincadeira
     def brincar(self, escolhaBrincadeira):
         if (self.tedio >= 0) and (self.tedio <= 100):    # Verifica o nível de tédio           
-            if isinstance(self, Aquati):    # Condição que verifica se a raça escolhida pelo usuário é a de água. Apenas ela beneficia com as brincadeiras.
-                if escolhaBrincadeira == 1:    # Brincadeira que envolve água diminui o tédio mais rápido
-                    self.tedio = max(0, self.tedio - 30)    # Garante que o valor não seja negativo, caso fique, será substituído para zero
-                    print(f"{self.nome} fez sua brincadeira favorita! 🤩")
-                elif escolhaBrincadeira in [2, 3, 4]:    # Brincadeiras 'favoritas' de outras raças
-                    self.tedio = max(0, self.tedio - 15)
-                elif escolhaBrincadeira == 5:    # Brincadeira genérica
-                    self.tedio = max(0, self.tedio - 10)
-                elif escolhaBrincadeira == 6:    # Cancelar escolha de brincadeiras
-                    print("Brincadeira cancelada.")
-            else:    # Outras raças
-                if escolhaBrincadeira in [1, 2, 3, 4]:
-                    self.tedio = max(0, self.tedio - 15)
-                elif escolhaBrincadeira == 5:
-                    self.tedio = max(0, self.tedio - 10)
-                elif escolhaBrincadeira == 6:
-                    print("Brincadeira cancelada")
+            if escolhaBrincadeira in [1, 2, 3, 4]:
+                self.tedio = max(0, self.tedio - 15)
+            elif escolhaBrincadeira == 5:
+                self.tedio = max(0, self.tedio - 10)
+            elif escolhaBrincadeira == 6:
+                print("Brincadeira cancelada")
         else:
             print(f"{self.nome} não está com tédio.") 
-        print(f"Nível de tédio: {self.fome}")    # Informação para o usuário
+        print(f"Nível de tédio: {self.tedio}")    # Informação para o usuário
 
     
     # Verifica as condições de alimentação
     def alimentar(self, escolhaComida):
         if (self.fome >= 0) and (self.fome <= 100):    # Verifica o nível de fome
-            if isinstance(self, Fuegui):     # Condição que verifica se a raça escolhida pelo usuário é a de fogo. Apenas ela beneficia com a alimentação.
-                if escolhaComida in [1, 2, 3]:
-                    self.fome = max(0, self.fome - 15)
-                elif escolhaComida == 4:
-                    self.fome = max(0, self.fome - 25)
-                    print(f"{self.nome} comeu sua comida favorita! 🤩")
-                elif escolhaComida == 5:
-                    self.fome = max(0, self.fome - 10)
-                elif escolhaComida == 6:
-                    print("Alimentação cancelada.")
-            else:    # Outras raças 
-                if escolhaComida in [1, 2, 3, 4]:
-                    self.fome = max(0, self.fome - 15)
-                elif escolhaComida == 5:
-                    self.fome = max(0, self.fome - 10)
-                elif escolhaComida == 6:
-                    print("Alimentação cancelada")
+            if escolhaComida in [1, 2, 3, 4]:
+                self.fome = max(0, self.fome - 15)
+            elif escolhaComida == 5:
+                self.fome = max(0, self.fome - 10)
+            elif escolhaComida == 6:
+                print("Alimentação cancelada")
         else:
             print(f"{self.nome} não está com fome!")
         print(f"Nível de fome: {self.fome}")    # Informação para o usuário
@@ -97,13 +78,13 @@ class Tamagoshi:
         humor = 100 - ((self.fome + self.tedio) / 2)
         # Condição que define o humor 
         if humor <= 25:
-            print("Humor: Triste 😭")
+            print("🙄 Humor: Triste 😭")
         elif humor <= 50:
-            print("Humor: Estável 😐")
+            print("🙄 Humor: Estável 😐")
         elif humor <= 75:
-            print("Humor: Bem ☺️")
+            print("🙄 Humor: Bem ☺️")
         else:
-            print("Humor: Muito feliz! 😁")
+            print("🙄 Humor: Muito feliz! 😁")
             
             
     # Verifica se está dormindo         
@@ -146,23 +127,74 @@ class Aquati(Tamagoshi):
     def __init__(self, nome):
         super().__init__(nome)    # Chamada do construtor da classe mãe  
         self.hidratacao = 100    # Definição de atributos específicos da raça
-        
+
+    def nadar(self):
+        self.tedio -= 15
+        self.hidratacao += 10
+        print(f"{self.nome} nadou e está mais feliz! 🐟")
+
     # Necessidade específica da raça
     def hidratar(self):
         self.hidratacao += 25
         print(f" {self.nome} bebeu água! 💧")
-        
-        
+
+
+
 # Raça de fogo
 class Fuegui(Tamagoshi):
     def __init__(self, nome):
         super().__init__(nome)    # Chamada do construtor da classe mãe
         self.calor = 100    # Definição de atributos específicos da raça
-        
+
      # Necessidade específica da raça
     def esquentar(self):
         self.calor += 25
         print(f"{self.nome} se esquentou e se sente melhor! ♨️")
+
+    def soltarFogo(self):
+        self.tedio -= 30
+        self.energia -= 20
+        print(f"{self.nome} soltou fogo! 🔥")
+
+
+
+# Raça de ar
+class Ari(Tamagoshi):
+    def __init__(self, nome):
+        super().__init__(nome)
+        self.refrescancia = 20 # MUDAR!
+
+    def pipar(self):
+        self.refrescancia += 30
+        self.tedio = max(0, self.tedio - 20)  # Correr alivia o tédio | '0' é para que pare de tirar quando o tédio atingir 0
+        print(f"{self.nome} soltou pipa ao AR livre e se sente melhor! 🍃")
+
+    def planar(self):
+        self.tedio = max(0, self.tedio - 20)
+        self.idade += 0.5
+        print(f"{self.nome} planou no céu e se sente melhor! ☁️")
+
+
+
+# Raça de terra
+class Terru(Tamagoshi):
+    def __init__(self, nome):
+        super().__init__(nome)
+        self.resistencia = 100  # Atributo especial da raça
+
+    def conexaoNatureza(self):
+        if self.fome <= 80:
+            self.saude = min(100, self.saude + 25)
+            self.tedio = max(0, self.tedio - 10)
+            print(f"{self.nome} se conectou com a natureza e se sente renovado! 🌱")
+        else:
+            print(f"{self.nome} está com fome demais para se concentrar! 🍽️")
+
+
+    def muralhaPedra(self):
+        self.saude = min(100, self.saude + 10)
+        self.idade = max(0, self.idade - 0.2)
+        print(f"{self.nome} criou uma muralha protetora e ganhou tempo! 🧱")
         
         
 # Raça de ar
@@ -171,52 +203,3 @@ class Fuegui(Tamagoshi):
 # Raça de terra
 
         
-# ====== Funcionamento do jogo ======
-def main():
-    # Interações iniciais
-    print(f"{25 * '='}\n  🌱🌬️  PET VIRTUAL 💧🔥 \n")
-    nome = input("=> Informe o nome do seu novo pet: ")
-    
-    escolhaRaca = int(input(f"=> Escolha a raça de {nome}:\n\n1 - Aquati (Água 💧)\n  * Possui vantagem com as brincadeiras\n  * Necessidade especial: Hidratação\n\n2 - Fuegui (Fogo 🔥)\n  * Possui vantagem com a alimentação\n  * Necessidade especial: Calor\n\n3 - \n\n4 -\n\nInforme sua escolha (1, 2, 3 ou 4): "))
-    # Condição para raça específica
-    if escolhaRaca == 1:
-        pet = Aquati(nome)
-    elif escolhaRaca == 2:
-        pet = Fuegui(nome)
-
-    
-    # Looping principal do jogo
-    while True:
-        
-        pet.statusPet()
-        pet.acoes()
-        
-        
-        
-        escolhaAcao = int(input("x "))
-
-    
-
-
-
-
-main()
-
-
-
-
-
-
-# Retirar daqui
-# Listas (ação, brincadeiras, comida)
-                    # Água            Terra               Ar                Fogo               
-listaBrincadeiras = ["Ir na piscina", "Castelo de areia", "Soltar pipa", "Jogar queimada", "Video game", "Cancelar escolha"]
-listaComidas = ["Gelatina", "Batata", "Pipoca", "Pimenta", "Sorvete", "Cancelar alimentação"]
-
-
-
-
-
-
-escolhaBrincadeiras = int(input(f"===== BRINCADEIRAS 😁 =====\nEscolha uma das brincadeiras a seguir:\n1 - 🏊‍♀️ {listaBrincadeiras[0]}\n2 - 🏰 {listaBrincadeiras[1]}\n3 - 🪁 {listaBrincadeiras[2]}\n4 - 🤾‍♀️ {listaBrincadeiras[3]}\n5 - 🎮 {listaBrincadeiras[4]}\n6 - ❌{listaBrincadeiras[5]}\n===============\nEscolha => "))
-escolhaComida = int(input(f"===== COMIDAS 😋 =====\nEscolha uma das comidas a seguir:\n1 - 🍮 {listaComidas[0]}\n2 - 🥔 {listaComidas[1]}\n3 - 🍿 {listaComidas[2]}\n4 - 🌶 {listaComidas[3]}\n5 - 🍦 {listaComidas[4]}\n6 - ❌ {listaComidas[5]}\n===============\nEscolha => "))          
---
+++ main.py
@@ -0,0 +1,60 @@
+from Tamagoshi_julya import *
+
+# ====== Funcionamento do jogo ======
+def main():
+    # Interações iniciais
+    print(f"{25 * '='}\n  🌱🌬️  PET VIRTUAL 💧🔥 \n")
+    nomeUsuario = input("Olá! Qual o seu nome? ")
+
+    print(f"Seja bem-vindo(a) {nomeUsuario}!\nVamos criar o seu bichinho virtual!\n")
+    nome = input("=> Informe o nome do seu novo pet: ")
+
+    print("Que nome lindo!\n\nAgora, escolha a raça dele(a):")
+    escolhaRaca = int(input(f"=> Escolha a raça de {nome}:\n\n1 - Aquati (Água 💧)\n  * Possui vantagem com as brincadeiras\n  * Necessidade especial: Hidratação\n\n2 - Fuegui (Fogo 🔥)\n  * Possui vantagem com a alimentação\n  * Necessidade especial: Calor\n\n3 - \n\n4 -\n\nInforme sua escolha (1, 2, 3 ou 4): "))
+    
+    # Condição para raça específica
+    if escolhaRaca == 1:
+        pet = Aquati(nome)
+    elif escolhaRaca == 2:
+        pet = Fuegui(nome)
+
+    print("...")
+    print("...")
+    print("Seu pet chegou!")
+
+    pet.statusPet()
+
+
+    # Looping principal do jogo
+    while True:
+        
+        print("\nO que deseja fazer agora?")
+        pet.acoes()
+        escolhaAcao = int(input(f"===== AÇÕES =====\nO que você deseja fazer agora?\n1 - 🍳 {listaAcao[0]}\n2 - 🎈 {listaAcao[1]}\n3 - 🕓 {listaAcao[2]}\n{listaAcao[3]}\n5 - 🔋 {listaAcao[4]}\n6 - ❌ {listaAcao[5]}\nEscolha => "))
+        
+        if escolhaAcao == 1:
+            escolhaComida = int(input(f"===== COMIDAS 😋 =====\nEscolha uma das comidas a seguir:\n1 - 🍮 {listaComidas[0]}\n2 - 🥔 {listaComidas[1]}\n3 - 🍿 {listaComidas[2]}\n4 - 🌶 {listaComidas[3]}\n5 - 🍦 {listaComidas[4]}\n6 - ❌ {listaComidas[5]}\n===============\nEscolha => "))
+            pet.alimentar(escolhaComida)
+
+        elif escolhaAcao == 2:
+            escolhaBrincadeiras = int(input(f"===== BRINCADEIRAS 😁 =====\nEscolha uma das brincadeiras a seguir:\n1 - 🏊‍♀️ {listaBrincadeiras[0]}\n2 - 🏰 {listaBrincadeiras[1]}\n3 - 🪁 {listaBrincadeiras[2]}\n4 - 🤾‍♀️ {listaBrincadeiras[3]}\n5 - 🎮 {listaBrincadeiras[4]}\n6 - ❌{listaBrincadeiras[5]}\n===============\nEscolha => "))
+            pet.brincar(escolhaBrincadeiras)
+        elif escolhaAcao == 3:
+            pet.tempoPassando()
+        elif escolhaAcao == 4:
+            if isinstance(pet, Aquati):
+                pet.hidratar()
+            elif isinstance(pet, Fuegui):
+                pet.esquentar()
+        elif escolhaAcao == 5:
+            pet.statusPet()
+        elif escolhaAcao == 6:
+            pet.dormir()
+        
+
+    
+
+
+
+
+main()
